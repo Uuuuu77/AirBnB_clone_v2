8e015dfa1c69bcadf1f0df354e25ee2ad4aa5847
John Njuguna <johnmnjuguna89@gmail.com>
2023-11-20 05:58:58 -0800

    Update state model

--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    __tablename__ = "states"
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete-orphan',
+                              backref='state')
+
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """ Returns list of City instances """
+            from models.city import City
+            from models import storage
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
