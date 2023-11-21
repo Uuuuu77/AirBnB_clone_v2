d5304c6aa7f301009d7e6a11b6dc2a02b370a73e
Benson Mwaura <125448975+Bensling@users.noreply.github.com>
2023-11-21 18:47:54 +0300

    setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,9 @@
--- Add database hbnb_dev_db.
--- Add new user hbnb_dev (in localhost).
--- The password of hbnb_dev should be set to hbnb_dev_pwd.
--- hbnb_dev should have all privileges on the database hbnb_dev_db.
--- hbnb_dev should have SELECT privilege on the database performance_schema.
-
-CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+-- Creating hbnb_dev_db database
+-- Privileges for new user (hbnb_dev)
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+USE hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
