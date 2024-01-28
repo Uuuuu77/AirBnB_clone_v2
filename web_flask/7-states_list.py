0f1b3f029f415b4d8f437967d20cae894002bcac
John Njuguna <johnmnjuguna89@gmail.com>
2024-01-28 10:51:17 -0800

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Starts a Flask eb application.
+"""
+
+from flask import Flask, render_template
+from models import strorage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ Remove the current SQLAlchemy Session after each request. """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ display a HTML page [inside the tag BODY) """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    """Listening on 0.0.0.0, port 5000"""
+    app.run(host='0.0.0.0', port=5000)
